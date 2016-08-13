4031d1024f1d6a881c0189336daecf27583d85eb
hesparza <hiram.esparza@gmail.com>
2016-08-13 11:43:17 -0500

    finally the detail component is working

--- frontEnd/learningAngular/APM/app/products/product-detail.component.ts
+++ frontEnd/learningAngular/APM/app/products/product-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component } from 'angular2/core';
-import { Router, RouteParams } from 'angular2/router'
+import { Router, RouteParams } from 'angular2/router';
+import { Observable } from 'rxjs/Observable';
 
-import { ProductListService } from './product-list.service'
-import { IProduct } from './product'
+import { ProductListService } from './product-list.service';
+import { IProduct } from './product';
+import { StarComponent } from '../shared/star.component';
 
 @Component({
-    templateUrl: 'app/products/product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
 })
 
 export class ProductDetailComponent {
@@ -15,8 +18,6 @@ export class ProductDetailComponent {
 
     constructor(private _routeParams: RouteParams, private _router: Router,
                 private _productListService: ProductListService) {
-        let id = +this._routeParams.get('id');
-        this.pageTitle += `: ${id}`;
     }
 
     onBack(): void {
@@ -24,7 +25,7 @@ export class ProductDetailComponent {
     }
 
     ngOnInit() {
-        if (!this.product) {
+        if(!this.product) {
             let id = +this._routeParams.get('id');
             // this.pageTitle += `: ${id}`;
             this.getProduct(id);
@@ -32,10 +33,24 @@ export class ProductDetailComponent {
     }
 
     getProduct(id: number) {
-        this._productListService.getProductById(id)
+        console.log('the id: ' + id);
+        // this._productListService.getProducts()
+        //     .subscribe(
+        //     product => this.product = product,
+        //     error => this.errorMessage = <any>error);
+
+        // this._productListService.getProducts()
+        //     .subscribe(function(result: IProduct[]) {
+        //         console.log('.......gathering the result......');
+        //         this.product = result[0];
+        //         console.log('And this is the product: ' + this.product);
+        //     },
+        //     error => this.errorMessage = <any>error);
+
+        this._productListService.getProduct(id)
             .subscribe(
-            product => this.product = product,
-            error => this.errorMessage = <any>error);
+                product => this.product = product,
+                error => this.errorMessage = <any>error);
     }
 
 }
--- frontEnd/learningAngular/APM/app/products/product-list.service.ts
+++ frontEnd/learningAngular/APM/app/products/product-list.service.ts
@@ -8,6 +8,7 @@ import { IProduct } from './product';
 @Injectable()
 export class ProductListService {
     private _getProductsUrl = 'api/products/productsa.json';
+    private _errorMessage: string;
 
     constructor(private _http: Http) {
 
@@ -20,9 +21,34 @@ export class ProductListService {
                    .catch(this.handleError);
     }
 
-    getProductById(id: number): Observable<IProduct> {
+    // getProductById(id: number): Observable<IProduct> {
+    //      this.getProducts()
+    //         .map((products: IProduct[]) => products.find(p => p.productId === id))
+    //         .subscribe(product => this.product = product, 
+    //                    error => this._errorMessage = <any>error);;
+    // }
+
+    getProductById(id: number): IProduct {
+        let products: IProduct[];
+        this._http.get(this._getProductsUrl)
+                   .map((response: Response) => <IProduct[]>response.json())
+                   .do(data => console.log('All: ' + JSON.stringify(data)))
+                   .catch(this.handleError)
+                   .subscribe(function(result: IProduct[]) {
+                                products = 
+                                result.filter(function(value: IProduct) {
+                                    return value.productId === id;
+                                }) 
+                                console.log('something');
+                             },
+                   error => this._errorMessage = <any>error);
+        return products[0];
+    }
+
+
+    getProduct(id: number): Observable<IProduct> {
         return this.getProducts()
-            .map((products: IProduct[]) => products.find(p => p.productId === id));
+                .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(error: Response) {
